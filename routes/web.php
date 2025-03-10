689586be998aa56da16f429d7ba9ddd469fbf071
yakanta <yakanta.k@gmail.com>
2025-03-10 12:16:06 +0700

    fix check

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('dashboard');
+    return redirect()->route('dashboard.index');
 });
 
 Route::get('login', [AuthController::class, 'login'])->name('login');
